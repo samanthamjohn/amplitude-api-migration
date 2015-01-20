904254f22170cb1bc17eb1f4ffe80b61eb91cf71
Samantha John <samantha.m.john@gmail.com>
2015-01-20 11:29:08 -0500

    Import daily data to amplitude

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,14 +11,9 @@ s3 = AWS::S3.new
 bucket = s3.buckets["hopscotchanalytics"]
 
 index = 0
-
-[-1, 0, 1].each do |day|
-  now = DateTime.now + day.days
-  date = now.strftime("%Y/%m/%-d")
-  bucket.objects.with_prefix("production/#{date}").each do |o|
-    if o.key.match("log")
-      AnalyticLog.create(sort_order: index, s3_object_key: o.key)
-      index += 1
-    end
+bucket.objects.with_prefix("production").each do |o|
+  if o.key.match("log")
+    AnalyticLog.create(sort_order: index, s3_object_key: o.key)
+    index += 1
   end
 end
---
+++ lib/tasks/import_daily_data.rake
@@ -0,0 +1,20 @@
+desc "Import data from our server to amplitude"
+task import_daily_data: :environment do
+  s3 = AWS::S3.new
+  bucket = s3.buckets["hopscotchanalytics"]
+
+  index = 0
+
+  (-1..1).each do |day|
+    now = DateTime.now + day.days
+    date = now.strftime("%Y/%m/%-d")
+    bucket.objects.with_prefix("production/#{date}").each do |o|
+      if o.key.match("log")
+        AnalyticLog.create(sort_order: index, s3_object_key: o.key)
+        index += 1
+      end
+    end
+  end
+
+  AnalyticLog.import_daily_data
+end
